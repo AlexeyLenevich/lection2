c6ee42cf381f32f9b5506885fab4590eaf4696e6
alexey lenevich <lenevich2012@mail.ru>
2024-03-12 17:52:37 +0300

    задание 4

---
+++ task4/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int[] arr = { 1, 2, 3, 4, 56 };
+int i = 0;
+int max = arr[0];
+
+while (i < arr.Length)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i = i + 1;
+}
+System.Console.WriteLine(max);
+
+max = arr [0];
+for (int j = 0; j < arr.Length; j++)
+{
+   if (arr[j] > max)
+    {
+        max = arr[j];
+    }
+}
+System.Console.WriteLine(max);
+
+
+max = arr [0];
+foreach(int e in arr)
+{
+   if (e > max)
+    {
+        max = e;
+    }  
+}
+System.Console.WriteLine(max);
