b23c353d8ced8d0fa04d1cd243267780dd45bbea
alexey lenevich <lenevich2012@mail.ru>
2024-03-12 17:12:20 +0300

    заадние 3

---
+++ task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// int n = 10;
+int[] arr  = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int i = 1;
+// bool IsChet(int[] arr)
+// {
+//     if ((arr[i]) % 2 == 0)
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
+
+while (i < arr.Length)
+
+{
+    if ((arr[i]) % 2 == 0)
+        // System.Console.WriteLine(arr [i]);
+        // System.Console.WriteLine(" ");
+        System.Console.Write($"{arr[i]} "); 
+    i = i + 1;    
+}
